0296176fe70e7d7ae6b9d1d5fdd5e70c17f4bb34
dexterleow <gdleow@gmail.com>
2019-01-29 23:18:06 +0800

    Add search user input

--- github-search/src/components/SearchInput.vue
+++ github-search/src/components/SearchInput.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="">
+  <div class>
     <h1>{{ msg }}</h1>
+    <input type="text" v-model="username" v-on:keydown.13="search">
+    <div class="loading">Searching GitHub for "{{ username }}"...</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchInput',
+  name: "SearchInput",
+  data: function() {
+    // declare username with an empty value
+    return {
+      username: ""
+    };
+  },
   props: {
     msg: String
+  },
+  methods: {
+    search: function() {
+      // ...
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
