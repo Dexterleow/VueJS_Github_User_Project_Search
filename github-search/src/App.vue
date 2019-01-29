7bfc710d565c5c9323b9441c0dc9f4e54d40830d
dexterleow <gdleow@gmail.com>
2019-01-29 23:04:59 +0800

    edit default hello world component to search

--- github-search/src/App.vue
+++ github-search/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SearchInput msg="Welcome to VueJS Github User Search"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchInput from './components/SearchInput.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SearchInput
   }
 }
 </script>
